d5763865cb9a96418a0a67ffbd6b317cf305f3f3
trevery <canjia.mail@gmail.com>
2019-01-02 10:23:10 +0800

    argument is fun, you can use python getUrl.py for default to getbook download urls on newest page.

--- getUrl.py
+++ getUrl.py
@@ -4,7 +4,8 @@
 #from urllib.request import urlopen
 import requests
 from bs4 import BeautifulSoup
-
+import sys
+import time
 ## generate page url
 
 def urlopen(url):
@@ -16,13 +17,14 @@ def urlopen(url):
 	r = requests.get(url, headers=headers)
 	return r.text
 
-def generate_pages(subTitle,pageNum):
+def generate_pages(subTitle,fromPage,toPage):
 	'''
 	return  page sites' url list
 	'''
 	pages = []
-	for i in range(1,pageNum+1):
-		pages.append('http://www.allitebooks.com'+subTitle+'/page/'+str(i))
+	if(fromPage > 0 and fromPage<toPage):
+		for i in range(fromPage,toPage+1):
+			pages.append('http://www.allitebooks.com'+subTitle+'/page/'+str(i))
 	return pages
 
 
@@ -63,10 +65,10 @@ def get_book_urls(bookSite):
 	return bookURLs    
 
 
-def get_all_book_urls(pageNum=1,subTitle=''):
+def get_all_book_urls(fromPage=1, toPage=1, subTitle=''):
 	bookSites = []
 	bookURLs = []
-	pages = generate_pages(subTitle,pageNum)
+	pages = generate_pages(subTitle,fromPage, toPage)
 	
 	for page in pages:
 		bookSiteOfOnePage=get_book_sites_of_one_page(page)
@@ -75,15 +77,63 @@ def get_all_book_urls(pageNum=1,subTitle=''):
 	for bookSite in bookSites:
 		book_urls=get_book_urls(bookSite)
 		bookURLs += book_urls
+		
 	for bookURL in bookURLs:
 		print(bookURL)
 		
+	#with open(filename, 'w') as f:
+	#	f.write(bookURLs)		
+	
 		
 def main():
-	get_all_book_urls()
+	if(len(sys.argv) == 4):
+		'''
+		python getUrl.py 1, 100, programming
+		from page 1 to page in subject programming
+		'''
+		subTitle = str(sys.argv[3])
+		fromPage = int(sys.argv[1])
+		toPage = int(sys.argv[2])
+		get_all_book_urls(fromPage, toPage, subTitle)
+	
+	if(len(sys.argv) == 3):
+		'''
+		python getUrl.py 1 100
+		from page 1 to page 100
+		'''
+		subTitle = ''
+		fromPage = int(sys.argv[1])
+		toPage = int(sys.argv[2])
+		#filename = subTitle="-"+str(pageNum)+".txt"
+		get_all_book_urls(fromPage, toPage, subTitle)
+		
+	elif(len(sys.argv) == 2):
+		'''
+		python getUrl.py 10
+		from page 10 to page 10
+		only download books on page 10
+		'''
+		fromPage = int(sys.argv[1])
+		toPage = fromPage + 1
+		subTitle = ''
+		#filename = "All-"+str(pageNum)+".txt"
+		get_all_book_urls(fromPage, toPage, subTitle)
+		
+	elif(len(sys.argv)== 1):
+		fromPage = 1
+		toPage = 2
+		subTitle = ''
+		
+		#filename = "All-"+"1"+"-"+time.strftime('%Y-%m-%d', time.localtime())+".txt"
+		get_all_book_urls(fromPage, toPage, subTitle)
+	else:
+		print("Error, too many arguments")
+		
+	
 	
 if __name__ == '__main__':
 	
+	#filename = ''
 	main()
 	
 	
